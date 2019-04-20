01013b7ba39afdf98cfecc1829b562735d722995
Trina <32879876+cati-123-199@users.noreply.github.com>
2019-04-20 11:46:44 +0100

    correçao guiao1

--- guioes/guiao1.c
+++ guioes/guiao1.c
@@ -2,6 +2,8 @@
 #include <sys/stat.h>
 #include <fcntl.h> //O_RDONLY, O_WRONLY, O_CREAT, O_TRUNC, O_*
 #include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 
 // 0 = stdin  /  STDIN_FILENO
 // 1 = stdout /  STDOUT_FILENO
@@ -31,8 +33,71 @@ void ex1(){
     close(fd);
 }
 
+void ex2_mycat1(){
+    int n;
+    char c;
+    while((n = read(0, &c, 1)) > 0)
+        write(1, &c, n);
+}
+
+void ex3_mycat2(int N){
+    int n;
+    char c[N];
+    while((n = read(0, c, N)) > 0)
+        write(1, c, n);
+}
+
+ssize_t ex5_readln(int fildes, char *buf, size_t nbyte){
+    int n, i = 0;
+    char c;
+    while((n = read(fildes, &c, 1)) > 0 && i < nbyte && c != '\n')
+        buf[i++] = c;
+    buf[i++] = '\n';
+    return i;
+}
+
+void ex6_nl(){
+    /*
+    int n, line = 1;
+    char c[1024];
+
+    while((n = read(0, c, 1024))>0)
+    {
+        write(1. , &line, 1);
+        write(1, c, n);
+        if(c[n-1] == '\n')
+            line++;
+    }
+    */
+    int n , line = 1;
+    char buf[1024], str[12];
+    while((n = ex5_readln(0, buf, 1024)) > 0) 
+    {
+        if(n > 1)
+        {
+            sprintf(str, "%d  ", line);
+            write(1, str, strlen(str));
+            write(1, buf, n);
+            line++;
+        }
+    }
+}
+
 int main(int argc, char* argv[])
 {
-    ex1();
+    // ex1();
+    // ex2_mycat1();
+    // ex3_mycat2(8);
+
+    /*
+    int fd = open("a.txt", O_RDONLY);
+    char buff[24];
+    int n = ex5_readln(fd,buff,16);
+    printf("n = %d\nbuff = %s\n", n, buff);
+    */
+
+    ex6_nl();
+
+
    return 0; 
 }
